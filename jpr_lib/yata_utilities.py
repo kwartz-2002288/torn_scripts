168544856513e7042c6f111f0db1681e6e7656ea
kwartz-2002288 <jp.roubin@gmail.com>
2025-11-11 09:49:50 +0100

    Remove unusued files

--- jpr_lib/yata_utilities.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def get_yata_targets(path):
-    """"
-    get the targets exported by YATA in path/target_list.json and load in a dictionary
-    """
-    with open(path+'target_list.json','r') as f:
-        return json.load(f)
